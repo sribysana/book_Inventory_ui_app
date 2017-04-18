776d50d050559f5b1920e9782ce0120caeec98d9
anandpotta <anandroy.foru@gmail.com>
2017-04-18 22:37:34 +0530

    book changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,15 +2,15 @@ import { combineReducers } from 'redux';
 import fuelSavings from './fuelSavingsReducer';
 import {routerReducer} from 'react-router-redux';
 import userReducer from '../common/user/userReducer';
-import bookReducer from '../components/book/bookReducer';
+import bookPageReducer from '../components/book/bookPageReducer';
 import authorReducer from '../components/author/authorReducer';
 
 const rootReducer = combineReducers({
   fuelSavings,
   userReducer,
-  bookReducer,
+  bookPageReducer,
   authorReducer,
-  routing: routerReducer
+  routing: routerReducer,
 });
 
 export default rootReducer;
